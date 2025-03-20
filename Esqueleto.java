f3066ff526007ebac87666c87260845ecc36b6fe
Luan Rebuli <yluanpvp@gmail.com>
2025-03-20 08:20:57 -0300

    create Esqueleto.java in classroom

---
+++ Esqueleto.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Esqueleto {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        int a;
+        float b;
+
+        System.out.println("Digite um valor para a");
+	    a = teclado.nextInt();
+		System.out.println("Digite um valor para b");
+	    b = teclado.nextFloat();
+	}
+
+}
